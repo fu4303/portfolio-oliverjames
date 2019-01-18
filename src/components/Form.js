91a945696dbafcbbd01f77cb80183c4b1855dee1
oliverjam <oliverjamesphillips@gmail.com>
2019-01-18 22:02:25 +0000

    Add party time and prototype dark mode

--- src/components/Form.js
+++ src/components/Form.js
@@ -53,3 +53,64 @@ export const Button = styled("button")`
     /* transform: translate(0.35rem, 0.35rem); */
   }
 `;
+
+export const AnimationButton = styled(Button)`
+  @media (prefers-reduced-motion: reduce) {
+    display: none;
+  }
+`;
+
+const Range = styled("input")`
+  &::-webkit-slider-runnable-track {
+    height: 0.25rem;
+    display: grid;
+    align-content: center;
+    background-color: var(--shade-60);
+  }
+  &::-webkit-slider-thumb {
+    appearance: none;
+    width: 1.5rem;
+    height: 1.5rem;
+    border-radius: 50%;
+    background-color: var(--shade-80);
+    box-shadow: 0.15rem 0.15rem 0 var(--shade-70);
+  }
+`;
+
+export function RangeInput({ id, label, ...rest }) {
+  return (
+    <Label htmlFor={id}>
+      {label}
+      <Range type="range" {...rest} />
+    </Label>
+  );
+}
+
+const Checkbox = styled("input")`
+  width: 1.75rem;
+  height: 1.75rem;
+  box-shadow: 0.25rem 0.25rem 0 var(--shade-70);
+  background-color: var(--shade-98);
+  &::after {
+    content: "✔︎";
+    display: block;
+    font-size: 1.5rem;
+    text-align: center;
+    color: var(--shade-60);
+    opacity: 0;
+  }
+  &:checked {
+    &::after {
+      opacity: 1;
+    }
+  }
+`;
+
+export function Toggle({ id, label, ...rest }) {
+  return (
+    <Label htmlFor="id">
+      {label}
+      <Checkbox type="checkbox" {...rest} />
+    </Label>
+  );
+}
--- src/components/ThemePicker.js
+++ src/components/ThemePicker.js
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { Button } from "./Form";
+import styled from "astroturf";
+import { RangeInput, AnimationButton, Toggle } from "./Form";
+
+const INITIAL_HUE = 20;
 
 function ThemePicker() {
-  const [hue, setHue] = useState(20);
+  const [hue, setHue] = useState(INITIAL_HUE);
+  const [party, setParty] = useState(false);
+  const [dark, setDark] = useState(false);
   function changeHue(event) {
     setHue(event.target.value);
   }
@@ -12,16 +17,42 @@ function ThemePicker() {
     },
     [hue]
   );
-  function partyTime() {
-    const interval = setInterval(() => setHue(prev => (prev + 10) % 360), 20);
-    setTimeout(() => clearInterval(interval), 5000);
-  }
+  useEffect(
+    () => {
+      let interval;
+      if (party) {
+        interval = setInterval(() => setHue(prev => (prev + 15) % 360), 32);
+      }
+      return () => {
+        clearInterval(interval);
+        setHue(INITIAL_HUE);
+      };
+    },
+    [party]
+  );
+  useEffect(
+    () => {
+      if (dark) {
+        document.documentElement.classList.add("dark");
+      } else {
+        document.documentElement.classList.remove("dark");
+      }
+    },
+    [dark]
+  );
   return (
     <form>
-      <input type="range" min={0} max={360} onChange={changeHue} value={hue} />
-      <Button onClick={partyTime} type="button">
+      <RangeInput
+        label="Hue"
+        min={0}
+        max={360}
+        onChange={changeHue}
+        value={hue}
+      />
+      <AnimationButton onClick={() => setParty(!party)} type="button">
         Party
-      </Button>
+      </AnimationButton>
+      <Toggle label="Dark mode" onChange={() => setDark(!dark)} value={dark} />
     </form>
   );
 }
